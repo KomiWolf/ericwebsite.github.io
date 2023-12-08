f4179874fca03a9b98c5043a3aa1ea8c149fa3c7
Eric XU <eric1.xu@epitech.eu>
2023-12-08 15:23:32 +0100

    [ADD] other button for top navigation

--- src/javascript/getAllJsonElement.js
+++ src/javascript/getAllJsonElement.js
@@ -43,6 +43,9 @@ function fetchJsonFileAndDisplayContent(filepath, id, jsonVariableName) {
 
 // Function to get all the json element to display
 function getAllJsonElement(filepath) {
-  fetchJsonFileAndDisplayContent(filepath, 'home', 'JsonNavigation.JsonHome');
-  fetchJsonFileAndDisplayContent(filepath, 'contact', 'JsonNavigation.JsonContact');
+  fetchJsonFileAndDisplayContent(filepath, 'top-nav-home', 'JsonNavigation.JsonHome');
+  fetchJsonFileAndDisplayContent(filepath, 'top-nav-company', 'JsonNavigation.JsonCompany');
+  fetchJsonFileAndDisplayContent(filepath, 'top-nav-product', 'JsonNavigation.JsonOurProducts');
+  fetchJsonFileAndDisplayContent(filepath, 'top-nav-news', 'JsonNavigation.JsonNews');
+  fetchJsonFileAndDisplayContent(filepath, 'top-nav-contact', 'JsonNavigation.JsonContactUs');
 }
