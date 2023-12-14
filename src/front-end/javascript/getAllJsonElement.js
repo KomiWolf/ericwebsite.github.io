0ea31c86b4e08ef084248a7661daef7a76a98c58
Eric XU <eric1.xu@epitech.eu>
2023-12-14 12:19:45 +0100

    [ADD] a default icon to all html metadata pages | [REMOVE] all element in getAllJsonElement.js file that don't exist anymore in html code

--- src/front-end/javascript/getAllJsonElement.js
+++ src/front-end/javascript/getAllJsonElement.js
@@ -9,11 +9,4 @@ function getAllJsonElement(filepath) {
   fetchJsonFileAndDisplayContent(filepath, 'top-nav-product', 'JsonNavigation.JsonOurProducts');
   fetchJsonFileAndDisplayContent(filepath, 'top-nav-news', 'JsonNavigation.JsonNews');
   fetchJsonFileAndDisplayContent(filepath, 'top-nav-contact', 'JsonNavigation.JsonContactUs');
-
-  // Top Navigation menu button
-  fetchJsonFileAndDisplayContent(filepath, 'top-nav-home-menu', 'JsonNavigation.JsonHome');
-  fetchJsonFileAndDisplayContent(filepath, 'top-nav-company-menu', 'JsonNavigation.JsonCompany');
-  fetchJsonFileAndDisplayContent(filepath, 'top-nav-product-menu', 'JsonNavigation.JsonOurProducts');
-  fetchJsonFileAndDisplayContent(filepath, 'top-nav-news-menu', 'JsonNavigation.JsonNews');
-  fetchJsonFileAndDisplayContent(filepath, 'top-nav-contact-menu', 'JsonNavigation.JsonContactUs');
 };
