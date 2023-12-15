0db15b6c3d463af214002129e70e4ed0e03cdc72
Eric XU <eric1.xu@epitech.eu>
2023-12-15 16:15:56 +0100

    [ADD] all

---
+++ src/front-end/javascript/getAllJsonElement.js
@@ -0,0 +1,23 @@
+// Function to get all the json element to display
+function getAllJsonElement(filepath, whichPage) {
+  // Header contact
+  fetchJsonFileAndDisplayContent(filepath, 'header-address', 'JsonHeaderContact.JsonAddress');
+
+  // Logo
+  fetchJsonFileAndDisplayContent(filepath, 'logo-top', 'JsonNavigation.JsonLogoTop');
+  fetchJsonFileAndDisplayContent(filepath, 'logo-bottom', 'JsonNavigation.JsonLogoBottom');
+
+  // Top Navigation button
+  fetchJsonFileAndDisplayContent(filepath, 'top-nav-home', 'JsonNavigation.JsonHome');
+  fetchJsonFileAndDisplayContent(filepath, 'top-nav-company', 'JsonNavigation.JsonCompany');
+  fetchJsonFileAndDisplayContent(filepath, 'top-nav-product', 'JsonNavigation.JsonOurProducts');
+  fetchJsonFileAndDisplayContent(filepath, 'top-nav-news', 'JsonNavigation.JsonNews');
+  fetchJsonFileAndDisplayContent(filepath, 'top-nav-contact', 'JsonNavigation.JsonContactUs');
+
+  if (whichPage === 'HomePage') {
+    // Home page element
+    fetchJsonFileAndDisplayContent(filepath, 'company-card-title', 'JsonHomePage.JsonCompanyCard.JsonCompanyCardTitle');
+    fetchJsonFileAndDisplayContent(filepath, 'company-card-description', 'JsonHomePage.JsonCompanyCard.JsonCompanyMiniDescription');
+    fetchJsonFileAndDisplayContent(filepath, 'Learn-more-button', 'JsonHomePage.JsonCompanyCard.JsonLearnMoreButton');
+  };
+};
